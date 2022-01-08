635ef3ded3b8a6f5a1096238dd1c4f29537f4b9f
Douglas <douglasecano1@gmail.com>
2022-01-08 11:45:31 -0500

    New updates to map and added cities folder

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -14,13 +14,18 @@ let map = L.map("mapid", {
     radius: 100
  }).addTo(map);
 
- // Add a circle to the map
+// Get data from cities.js
+let cityData = cities;
 
- L.circleMarker([34.0522, -118.2437], {
-     radius: 300,
-     color: "green",
-     fillColor: '#ffffa1'
- }).addTo(map);
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
 
 
 // We create the tile layer that will be the background of our map.
